1e5d523fa0dc5070d2f57ef6c70f386d2956c670
HeatherWelch <heather.welch@noaa.gov>
2017-08-02 11:49:24 -0700

    written thru to plotting

--- code/quantify_OO.R
+++ code/quantify_OO.R
@@ -2,14 +2,17 @@
 ## # pixels in SA = 12936 ncell(studyarea)
 
 library(raster)
+library(ggplot2)
+library(reshape)
+library(gridExtra)
 
-#######commands
-
+####### general objects
 Lagged_dir="/Volumes/SeaGate/ERD_DOM/EcoCast_CodeArchive_Sensitivity/EcoCastRuns/output/mean/Lagged" 
 LOO="/Volumes/SeaGate/ERD_DOM/EcoCast_CodeArchive_Sensitivity/EcoCastRuns/output/mean/LOO" 
 OO_dir="/Volumes/SeaGate/ERD_DOM/EcoCast_CodeArchive_Sensitivity/EcoCastRuns/output/mean/OO" 
 studyarea=raster("/Volumes/SeaGate/EcoCast_HW/EcoCastGit_private/EcoCast-private/Code/RShinyApp/data/sa_small.tif")
 
+###### define function
 quantify_OO=function(dir=OO_dir,studyarea){
   setwd(dir)
   
@@ -84,5 +87,56 @@ quantify_OO=function(dir=OO_dir,studyarea){
   }
   
   OO=rbind(empty_2012,empty_2015)
+  return(OO)
   
 }
+
+###### run function
+DF=quantify_OO(dir=OO_dir,studyarea=studyarea)
+write.csv(DF,"/Volumes/SeaGate/EcoCast_HW/EcoCastGit_Sensitivity_Hindcast/EcoCast-Sensitivity-Hindcast-Analyses/analysis_DFs/OO.csv")
+
+####### make some plots, segraged by month
+plot(DF$t.minus,DF$s.mean,cols=DF$month)
+qplot(t.minus, s.mean, colour = month,
+      data = DF)
+
+ggplot(DF, aes(t.minus, s.mean,colour = month)) + geom_point() + geom_smooth(method = "lm")
+ggplot(DF, aes(t.minus, s.SD,colour = month)) + geom_point() + geom_smooth(method = "lm")
+ggplot(DF, aes(t.minus, p.GT.5,colour = month)) + geom_point() + geom_smooth(method = "lm")
+ggplot(DF, aes(t.minus, p.GT.25,colour = month)) + geom_point() + geom_smooth(method = "lm")
+ggplot(DF, aes(t.minus, p.GT.1,colour = month)) + geom_point() + geom_smooth(method = "lm")
+
+####### make some plots, averaged across month
+DF_complete=DF[complete.cases(DF),]
+DF_complete=DF_complete[,c(4:9)]
+a=melt(DF_complete,id="t.minus")
+means=cast(a,t.minus~variable,mean)
+
+##make %
+means$p.GT.1=means$p.GT.1*100
+means$p.GT.25=means$p.GT.25*100
+means$p.GT.5=means$p.GT.5*100
+
+#normalizing between 1:0
+range01 <- function(x){(x-min(x))/(max(x)-min(x))} 
+means01=means
+means01=as.data.frame(lapply(means01,FUN=range01))
+means01$t.minus=means$t.minus
+
+###plotting
+s.mean=ggplot(means, aes(t.minus, s.mean)) + geom_point() + geom_line(colour="blue")+geom_text(aes(label=t.minus),hjust=1.3)
+a=s.mean+labs(x="Number of days lagged")+labs(y="Mean difference from zero lag")+ theme(panel.background = element_blank())+ theme(axis.line = element_line(colour = "black"))
+
+s.SD=ggplot(means, aes(t.minus, s.SD)) + geom_point() + geom_line(colour="blue")+geom_text(aes(label=t.minus),hjust=1.3)
+b=s.SD+labs(x="Number of days lagged")+labs(y="SD of difference from zero lag")+ theme(panel.background = element_blank())+ theme(axis.line = element_line(colour = "black"))
+
+p.GT.5=ggplot(means, aes(t.minus, p.GT.5)) + geom_point() + geom_line(colour="blue")+geom_text(aes(label=t.minus),hjust=1.3)
+c=p.GT.5+labs(x="Number of days lagged")+labs(y="% of pixels with > .5 difference from zero lag")+ theme(panel.background = element_blank())+ theme(axis.line = element_line(colour = "black"))
+
+p.GT.1=ggplot(means, aes(t.minus, p.GT.1)) + geom_point() + geom_line(colour="blue")+geom_text(aes(label=t.minus),hjust=1.3)
+d=p.GT.1+labs(x="Number of days lagged")+labs(y="% of pixels with > .1 difference from zero lag")+ theme(panel.background = element_blank())+ theme(axis.line = element_line(colour = "black"))
+
+png("/Volumes/SeaGate/EcoCast_HW/EcoCastGit_Sensitivity_Hindcast/EcoCast-Sensitivity-Hindcast-Analyses/analysis_DFs/OO_analysis.png",width=1100,height=1100,units='px',pointsize=35)
+grid.arrange(a,b,c,d,top="Lagged official output analysis")
+dev.off()
+
